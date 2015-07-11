d69ecb5432d555ccd1bb74060fac537a6035bc31
reachmehere89 <reachmehere89@gmail.com>
2015-07-11 21:45:17 +0530

    Added Test Class

---
+++ TestProject/src/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author Pranav Garg
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
